5dbc4780547781623d3cf25112f1b2efee7059ad
acc <edwvee@gmail.com>
2021-10-04 01:20:30 +0300

    http receiver: some tests

--- internal/receiver/htp_receiver_test.go
+++ internal/receiver/htp_receiver_test.go
@@ -1,10 +1,189 @@
 package receiver
 
-const defaultHttpReceiverBind = "localhost:8090"
+import (
+	"fmt"
+	"os"
+	"sync"
+	"testing"
+	"time"
 
-var defaultHttpReceiverConfig = Config{
-	Type: "http",
-	Bind: defaultHttpReceiverBind,
+	"github.com/edwvee/dbatcher/internal/inserter"
+	"github.com/edwvee/dbatcher/internal/table"
+	"github.com/edwvee/dbatcher/internal/tablemanager"
+	"github.com/valyala/fasthttp"
+)
+
+const bindEnvKey = "DBATCHER_HTTP_RECEIVER_TEST_BIND"
+
+var defaultHTTPReceiverBind = "localhost:8090"
+
+var defaultHTTPReceiverConfig Config
+
+func init() {
+	bind := os.Getenv(bindEnvKey)
+	if bind != "" {
+		defaultHTTPReceiverBind = bind
+	}
+	defaultHTTPReceiverConfig = Config{
+		Type: "http",
+		Bind: defaultHTTPReceiverBind,
+	}
+}
+
+func TestReceive(t *testing.T) {
+	rec := &HTTPReceiver{}
+	errChan := make(chan error)
+	inserters := map[string]inserter.Inserter{
+		"dummy": &inserter.DummyInserter{},
+	}
+	tmh := tablemanager.NewTableManagerHolder(errChan, inserters)
+	if err := rec.Init(defaultHTTPReceiverConfig, errChan, tmh); err != nil {
+		t.Errorf("shouldn't return error: %s", err.Error())
+	}
+
+	rec.Receive()
+	defer rec.Stop()
+	time.Sleep(time.Millisecond * 100)
+	select {
+	case <-errChan:
+		t.Fatal("there shouldn't be an error. server didn't start")
+	default:
+	}
+	code, _, err := fasthttp.Get(nil, "http://"+defaultHTTPReceiverBind+"/")
+	if err != nil {
+		t.Fatalf("got error trying to get: %s", err.Error())
+	}
+	if code == 0 {
+		t.Error("response code shouldn't be 0")
+	}
+
+	recShouldntStart := &HTTPReceiver{}
+	recShouldntStart.Init(defaultHTTPReceiverConfig, errChan, tmh)
+	recShouldntStart.Receive()
+	time.Sleep(time.Millisecond * 100)
+	select {
+	case <-errChan:
+	default:
+		t.Error("after starting second receiver there should be an error. cause it can't bind to the same address")
+	}
+}
+
+type selfSliceInserter struct {
+	data    []interface{}
+	dataMut sync.Mutex
+}
+
+func (si *selfSliceInserter) Init(c inserter.Config) error {
+	si.data = []interface{}{}
+	return nil
+}
+
+func (si *selfSliceInserter) Insert(t *table.Table) error {
+	si.dataMut.Lock()
+	for row := t.GetNextRow(); row != nil; row = t.GetNextRow() {
+		si.data = append(si.data, row)
+	}
+	si.dataMut.Unlock()
+
+	return nil
+}
+
+func (si *selfSliceInserter) TakeSlice() []interface{} {
+	si.dataMut.Lock()
+	defer si.dataMut.Unlock()
+	res := si.data
+	si.data = nil
+
+	return res
+}
+
+func TestHTTPReceiverByRequests(t *testing.T) {
+	rec := &HTTPReceiver{}
+	errChan := make(chan error)
+	ins := &selfSliceInserter{}
+	inserters := map[string]inserter.Inserter{
+		"first": ins,
+	}
+	tmh := tablemanager.NewTableManagerHolder(errChan, inserters)
+	if err := rec.Init(defaultHTTPReceiverConfig, errChan, tmh); err != nil {
+		t.Errorf("shouldn't return error: %s", err.Error())
+	}
+	rec.Receive()
+	defer rec.Stop()
+	time.Sleep(time.Millisecond * 100)
+	select {
+	case <-errChan:
+		t.Fatal("there shouldn't be an error. server didn't start")
+	default:
+	}
+
+	//wrong method
+	code, _, err := fasthttp.Get(nil, "http://"+defaultHTTPReceiverBind+"/")
+	if err != nil {
+		t.Fatalf("got error trying to get: %s", err.Error())
+	}
+	if code != 405 {
+		t.Errorf("code should be 405, got: %d", code)
+	}
+
+	//test invalid table
+	url := fmt.Sprintf("http://%s/?table=%s&fields=%s", defaultHTTPReceiverBind, "table", "field1,,field3")
+	code, _, err = fasthttp.Post(nil, url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if code != 400 {
+		t.Errorf("code should be 400, got: %d", code)
+	}
+	url = fmt.Sprintf("http://%s/?table=%s&fields=%s", defaultHTTPReceiverBind, "table", "field1,field2")
+
+	//invalid timeout_ms
+	url0 := url + "&timeout_ms=fasdf"
+	code, _, err = fasthttp.Post(nil, url0, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if code != 400 {
+		t.Errorf("code should be 400, got: %d", code)
+	}
+	url += "&timeout_ms=1000"
+
+	url0 = url + "&max_rows=dfsf"
+	code, _, err = fasthttp.Post(nil, url0, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if code != 400 {
+		t.Errorf("code should be 400, got: %d", code)
+	}
+	url += "&max_rows=10"
+
+	//yet not support persist
+	url0 = url + "&persist=1"
+	code, _, err = fasthttp.Post(nil, url0, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if code != 400 {
+		t.Errorf("code should be 400, got: %d", code)
+	}
+
+	client := fasthttp.Client{}
+	request := fasthttp.AcquireRequest()
+	response := fasthttp.AcquireResponse()
+
+	//unsuccsessfull insert
+	request.Header.SetMethod(fasthttp.MethodPost)
+	request.SetRequestURI(url)
+	request.SetBodyRaw([]byte("[[2]]"))
+	err = client.Do(request, response)
+	if err != nil {
+		t.Errorf("shouldn't be an error: %s", err.Error())
+	}
+	if code := response.StatusCode(); code != 400 {
+		t.Error("should be error")
+	}
+	//TODO: make successfull requests and test inserter's data
 }
 
 //freezed cause there is no way to shutdown fasthttp.Server with idle connectios yet
